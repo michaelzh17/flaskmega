b2a3e6bd5bb7f24d93a79765904564f399166446
michaelzh17 <macalzhang@gmail.com>
2018-11-05 11:10:17 -0500

    migration and templates update

---
+++ microblog/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # configuraton for database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
